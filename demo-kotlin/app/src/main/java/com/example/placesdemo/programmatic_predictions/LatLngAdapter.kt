f068b23b00631dbcaa7fffc8f57cde4820bcdb89
Chris Arriola <carriola@google.com>
2020-04-22 13:17:03 -0700

    Updating license headers.

--- demo-kotlin/app/src/main/java/com/example/placesdemo/programmatic_predictions/LatLngAdapter.kt
+++ demo-kotlin/app/src/main/java/com/example/placesdemo/programmatic_predictions/LatLngAdapter.kt
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package com.example.placesdemo.programmatic_predictions
 
 import com.google.android.gms.maps.model.LatLng
