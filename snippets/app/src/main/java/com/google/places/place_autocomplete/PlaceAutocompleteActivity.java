fac931db0eae9f4e3d7de3863a159aea7a3a9051
Chris Arriola <carriola@google.com>
2020-06-25 16:10:18 -0700

    chore: Create separate package for place autocomplete.

---
+++ snippets/app/src/main/java/com/google/places/place_autocomplete/PlaceAutocompleteActivity.java
@@ -0,0 +1,197 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.places.place_autocomplete;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.AutocompletePrediction;
+import com.google.android.libraries.places.api.model.AutocompleteSessionToken;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.api.model.TypeFilter;
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+import com.google.places.R;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class PlaceAutocompleteActivity extends AppCompatActivity {
+
+    private static final String TAG = PlaceAutocompleteActivity.class.getSimpleName();
+
+    private PlacesClient placesClient;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final String apiKey = "You API key";
+        // [START maps_places_get_started]
+        // Initialize the SDK
+        Places.initialize(getApplicationContext(), apiKey);
+
+        // Create a new PlacesClient instance
+        PlacesClient placesClient = Places.createClient(this);
+        // [END maps_places_get_started]
+    }
+
+    private void initAutocompleteSupportFragment() {
+        // [START maps_places_autocomplete_support_fragment]
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+            getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NotNull Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+            }
+
+
+            @Override
+            public void onError(@NotNull Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+        // [END maps_places_autocomplete_support_fragment]
+
+        // [START maps_places_autocomplete_location_bias]
+        autocompleteFragment.setLocationBias(RectangularBounds.newInstance(
+            new LatLng(-33.880490, 151.184363),
+            new LatLng(-33.858754, 151.229596)));
+        // [END maps_places_autocomplete_location_bias]
+
+        // [START maps_places_autocomplete_location_restriction]
+        autocompleteFragment.setLocationRestriction(RectangularBounds.newInstance(
+            new LatLng(-33.880490, 151.184363),
+            new LatLng(-33.858754, 151.229596)));
+        // [END maps_places_autocomplete_location_restriction]
+
+        // [START maps_places_autocomplete_type_filter]
+        autocompleteFragment.setTypeFilter(TypeFilter.ADDRESS);
+        // [END maps_places_autocomplete_type_filter]
+
+        List<Place.Field> fields = new ArrayList<>();
+        // [START maps_places_intent_type_filter]
+        Intent intent = new Autocomplete.IntentBuilder(
+            AutocompleteActivityMode.FULLSCREEN, fields)
+            .setTypeFilter(TypeFilter.ADDRESS)
+            .build(this);
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+        // [END maps_places_intent_type_filter]
+
+        // [START maps_places_autocomplete_country_filter]
+        autocompleteFragment.setCountries("AU", "NZ");
+        // [END maps_places_autocomplete_country_filter]
+    }
+
+    // [START maps_places_autocomplete_intent]
+    private static int AUTOCOMPLETE_REQUEST_CODE = 1;
+
+    // [START_EXCLUDE]
+    private void startAutocompleteIntent() {
+    // [END_EXCLUDE]
+        // Set the fields to specify which types of place data to
+        // return after the user has made a selection.
+        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
+
+        // Start the autocomplete intent.
+        Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN, fields)
+            .build(this);
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
+    // [END maps_places_autocomplete_intent]
+    }
+
+    // [START maps_places_on_activity_result]
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = Autocomplete.getPlaceFromIntent(data);
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
+                // TODO: Handle the error.
+                Status status = Autocomplete.getStatusFromIntent(data);
+                Log.i(TAG, status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    // [END maps_places_on_activity_result]
+
+    // [START maps_places_programmatic_place_predictions]
+    private void programmaticPlacePredictions(String query) {
+        // Create a new token for the autocomplete session. Pass this to FindAutocompletePredictionsRequest,
+        // and once again when the user makes a selection (for example when calling fetchPlace()).
+        AutocompleteSessionToken token = AutocompleteSessionToken.newInstance();
+
+        // Create a RectangularBounds object.
+        RectangularBounds bounds = RectangularBounds.newInstance(
+            new LatLng(-33.880490, 151.184363),
+            new LatLng(-33.858754, 151.229596));
+        // Use the builder to create a FindAutocompletePredictionsRequest.
+        FindAutocompletePredictionsRequest request = FindAutocompletePredictionsRequest.builder()
+            // Call either setLocationBias() OR setLocationRestriction().
+            .setLocationBias(bounds)
+            //.setLocationRestriction(bounds)
+            .setOrigin(new LatLng(-33.8749937,151.2041382))
+            .setCountries("AU", "NZ")
+            .setTypeFilter(TypeFilter.ADDRESS)
+            .setSessionToken(token)
+            .setQuery(query)
+            .build();
+
+        placesClient.findAutocompletePredictions(request).addOnSuccessListener((response) -> {
+            for (AutocompletePrediction prediction : response.getAutocompletePredictions()) {
+                Log.i(TAG, prediction.getPlaceId());
+                Log.i(TAG, prediction.getPrimaryText(null).toString());
+            }
+        }).addOnFailureListener((exception) -> {
+            if (exception instanceof ApiException) {
+                ApiException apiException = (ApiException) exception;
+                Log.e(TAG, "Place not found: " + apiException.getStatusCode());
+            }
+        });
+    }
+    // [END maps_places_programmatic_place_predictions]
+}
