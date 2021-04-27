64b6e71f93a8c2f1a690a1d3359113fb961262c5
Angela Yu <5506675+wangela@users.noreply.github.com>
2021-04-27 15:50:34 -0700

    fix: implement address form fill and add confirmation map

--- demo-java/app/src/gms/java/com/example/placesdemo/AutocompleteAddressActivity.java
+++ demo-java/app/src/gms/java/com/example/placesdemo/AutocompleteAddressActivity.java
@@ -7,7 +7,9 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewStub;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +25,8 @@ import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.AddressComponent;
+import com.google.android.libraries.places.api.model.AddressComponents;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.Autocomplete;
@@ -35,16 +39,24 @@ import java.util.List;
 /**
  * Activity for using Place Autocomplete to assist filling out an address form.
  */
+@SuppressWarnings("FieldCanBeLocal")
 public class AutocompleteAddressActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final String TAG = "ADDRESSAUTOCOMPLETE";
+    private static final String MAP_FRAGMENT_TAG = "map";
     private static final int AUTOCOMPLETE_REQUEST_CODE = 23487;
-    private PlacesClient placesClient;
+    private EditText address1Field;
+    private EditText address2Field;
+    private EditText cityField;
+    private EditText stateField;
+    private EditText postalField;
+    private EditText countryField;
     private LatLng coordinates;
     private SupportMapFragment mapFragment;
     private GoogleMap map;
     private Marker marker;
-    private final OnMapReadyCallback callbackObject = this;
+    private PlacesClient placesClient;
+    private View mapPanel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,66 +70,34 @@ public class AutocompleteAddressActivity extends AppCompatActivity implements On
         }
 
         setContentView(R.layout.autocomplete_address_activity);
-        EditText address1Field = findViewById(R.id.autocomplete_address1);
-        address1Field.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(MotionEvent.ACTION_UP == event.getAction()) {
-                    startAutocompleteIntent();
-                }
-
-                return true;
-            }
-        });
 
         // Retrieve a PlacesClient (previously initialized - see MainActivity)
         placesClient = Places.createClient(this);
 
-//        setupAutocompleteSupportFragment();
+        address1Field = (EditText) findViewById(R.id.autocomplete_address1);
+        address2Field = (EditText) findViewById(R.id.autocomplete_address2);
+        cityField = (EditText) findViewById(R.id.autocomplete_city);
+        stateField = (EditText) findViewById(R.id.autocomplete_state);
+        postalField = (EditText) findViewById(R.id.autocomplete_postal);
+        countryField = (EditText) findViewById(R.id.autocomplete_country);
+
+        // Attach an Autocomplete intent to the Address 1 EditText field
+        address1Field.setOnTouchListener((v, event) -> {
+            if(MotionEvent.ACTION_UP == event.getAction()) {
+                startAutocompleteIntent();
+            }
+
+            return true;
+        });
+
+        // Reset the form
+        Button resetButton = findViewById(R.id.autocomplete_reset_button);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                clearForm();
+            }
+        });
     }
-
-
-
-//    private void setupAutocompleteSupportFragment() {
-//        // Initialize the AutocompleteSupportFragment.
-//        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
-//                getSupportFragmentManager().findFragmentById(R.id.autocomplete_support_fragment_address);
-//
-//        // Specify the types of place data to return.
-//        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ADDRESS_COMPONENTS,
-//                Place.Field.LAT_LNG, Place.Field.VIEWPORT));
-//
-//        // Set up a PlaceSelectionListener to handle the response.
-//        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-//            @Override
-//            public void onPlaceSelected(@NonNull Place place) {
-//                // TODO: Get info about the selected place.
-//                Log.i(TAG, "Place: " + place.getAddressComponents());
-//
-//                coordinates = place.getLatLng();
-//                if (mapFragment == null) {
-//                    View mapPanel = ((ViewStub) findViewById(R.id.stub_map)).inflate();
-//                    GoogleMapOptions mapOptions = new GoogleMapOptions();
-//                    mapOptions.liteMode(true)
-//                            .mapToolbarEnabled(false);
-//                    mapFragment = SupportMapFragment.newInstance(mapOptions);
-//                    getSupportFragmentManager()
-//                            .beginTransaction()
-//                            .add(R.id.panel_map, mapFragment)
-//                            .commit();
-//                    mapFragment.getMapAsync(callbackObject);
-//                } else {
-//                    updateMap(coordinates);
-//                }
-//            }
-//
-//            @Override
-//            public void onError(@NonNull Status status) {
-//                // TODO: Handle the error.
-//                Log.i(TAG, "An error occurred: " + status);
-//            }
-//        });
-//    }
 
     private void startAutocompleteIntent() {
         // Set the fields to specify which types of place data to
@@ -138,20 +118,8 @@ public class AutocompleteAddressActivity extends AppCompatActivity implements On
                 Place place = Autocomplete.getPlaceFromIntent(data);
                 Log.i(TAG, "Place: " + place.getAddressComponents());
 
-                coordinates = place.getLatLng();
-                if (mapFragment == null) {
-                    View mapPanel = ((ViewStub) findViewById(R.id.stub_map)).inflate();
-                    GoogleMapOptions mapOptions = new GoogleMapOptions();
-                    mapOptions.mapToolbarEnabled(false);
-                    mapFragment = SupportMapFragment.newInstance(mapOptions);
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .add(R.id.panel_map, mapFragment)
-                            .commit();
-                    mapFragment.getMapAsync(callbackObject);
-                } else {
-                    updateMap(coordinates);
-                }
+                fillInAddress(place);
+
             } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
                 // TODO: Handle the error.
                 Status status = Autocomplete.getStatusFromIntent(data);
@@ -186,6 +154,106 @@ public class AutocompleteAddressActivity extends AppCompatActivity implements On
     private void updateMap(LatLng latLng) {
         marker.setPosition(latLng);
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f));
+        if (mapPanel.getVisibility() == View.GONE) {
+            mapPanel.setVisibility(View.VISIBLE);
+        }
     }
 
+    private void fillInAddress(Place place) {
+        AddressComponents components = place.getAddressComponents();
+        StringBuilder address1 = new StringBuilder();
+        StringBuilder postcode = new StringBuilder();
+
+        // Get each component of the address from the place details,
+        // and then fill-in the corresponding field on the form.
+        // Possible AddressComponent types are documented at https://goo.gle/32SJPM1
+        if (components != null) {
+            for(AddressComponent component : components.asList()) {
+                String type = component.getTypes().get(0);
+                switch(type) {
+                    case "street_number": {
+                        address1.insert(0, component.getName());
+                        break;
+                    }
+
+                    case "route": {
+                        address1.append(" ");
+                        address1.append(component.getShortName());
+                        break;
+                    }
+
+                    case "postal_code": {
+                        postcode.insert(0, component.getName());
+                        break;
+                    }
+
+                    case "postal_code_suffix": {
+                        postcode.append("-").append(component.getName());
+                        break;
+                    }
+
+                    case "locality":
+                        cityField.setText(component.getName());
+                        break;
+
+                    case "administrative_area_level_1": {
+                        stateField.setText(component.getShortName());
+                        break;
+                    }
+
+                    case "country":
+                        countryField.setText(component.getName());
+                        break;
+                }
+            }
+        }
+
+        address1Field.setText(address1.toString());
+        postalField.setText(postcode.toString());
+
+        // After filling the form with address components from the Autocomplete
+        // prediction, set cursor focus on the second address line to encourage
+        // entry of sub-premise information such as apartment, unit, or floor number.
+        address2Field.requestFocus();
+
+        // Add a map for visual confirmation of the address
+        coordinates = place.getLatLng();
+
+        // It isn't possible to set a fragment's id programmatically so we set a tag instead and
+        // search for it using that.
+        mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentByTag(MAP_FRAGMENT_TAG);
+
+        // We only create a fragment if it doesn't already exist.
+        if (mapFragment == null) {
+            mapPanel = (View) ((ViewStub) findViewById(R.id.stub_map)).inflate();
+            GoogleMapOptions mapOptions = new GoogleMapOptions();
+            mapOptions.mapToolbarEnabled(false);
+
+            // To programmatically add the map, we first create a SupportMapFragment.
+            mapFragment = SupportMapFragment.newInstance(mapOptions);
+
+            // Then we add it using a FragmentTransaction.
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.confirmation_map, mapFragment, MAP_FRAGMENT_TAG)
+                    .commit();
+            mapFragment.getMapAsync(this);
+        } else {
+            updateMap(coordinates);
+        }
+    }
+
+    private void clearForm() {
+        address1Field.getText().clear();
+        address2Field.getText().clear();
+        cityField.getText().clear();
+        stateField.getText().clear();
+        postalField.getText().clear();
+        countryField.getText().clear();
+        if (mapPanel != null) {
+            mapPanel.setVisibility(View.GONE);
+        }
+        address1Field.requestFocus();
+    }
 }
