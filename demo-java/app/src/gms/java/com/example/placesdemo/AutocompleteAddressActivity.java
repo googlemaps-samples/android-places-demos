cad6283636d819d29d0332a3da707d8497cb9995
Angela Yu <5506675+wangela@users.noreply.github.com>
2021-06-01 16:13:26 -0700

    feat: add proximity checking to autocomplete address form java sample (#190)
    * feat: add proximity checking to autocomplete address form java sample

--- demo-java/app/src/gms/java/com/example/placesdemo/AutocompleteAddressActivity.java
+++ demo-java/app/src/gms/java/com/example/placesdemo/AutocompleteAddressActivity.java
@@ -1,19 +1,29 @@
 package com.example.placesdemo;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewStub;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.placesdemo.model.AutocompleteEditText;
-import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
@@ -29,11 +39,13 @@ import com.google.android.libraries.places.api.model.AddressComponents;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.Autocomplete;
-import com.google.android.libraries.places.widget.AutocompleteActivity;
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static com.google.maps.android.SphericalUtil.computeDistanceBetween;
 
 /**
  * Activity for using Place Autocomplete to assist filling out an address form.
@@ -51,11 +63,28 @@ public class AutocompleteAddressActivity extends AppCompatActivity implements On
     private EditText postalField;
     private EditText countryField;
     private LatLng coordinates;
+    private boolean checkProximity = false;
     private SupportMapFragment mapFragment;
     private GoogleMap map;
     private Marker marker;
     private PlacesClient placesClient;
     private View mapPanel;
+    private LatLng deviceLocation;
+    private static final double acceptableProximity = 150;
+
+    private ActivityResultLauncher<Intent> startAutocomplete = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            (ActivityResultCallback<ActivityResult>) result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent intent = result.getData();
+                    Place place = Autocomplete.getPlaceFromIntent(intent);
+                    Log.d(TAG, "Place: " + place.getAddressComponents());
+
+                    fillInAddress(place);
+                } else if (result.getResultCode() == Activity.RESULT_CANCELED) {
+                    // The user canceled the operation.
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,10 +112,23 @@ public class AutocompleteAddressActivity extends AppCompatActivity implements On
         // Attach an Autocomplete intent to the Address 1 EditText field
         address1Field.setOnClickListener(v -> startAutocompleteIntent());
 
+        // Update checkProximity when user checks the checkbox
+        CheckBox checkProximityBox = findViewById(R.id.checkbox_proximity);
+        checkProximityBox.setOnCheckedChangeListener((view, isChecked) -> {
+            // Set the boolean to match user preference for when the Submit button is clicked
+            checkProximity = isChecked;
+        });
+
+        // Submit and optionally check proximity
+        Button saveButton = findViewById(R.id.autocomplete_save_button);
+        saveButton.setOnClickListener(v -> saveForm());
+
         // Reset the form
         Button resetButton = findViewById(R.id.autocomplete_reset_button);
         resetButton.setOnClickListener(v -> clearForm());
     }
+
+
 
     private void startAutocompleteIntent() {
         // Set the fields to specify which types of place data to
@@ -97,28 +139,7 @@ public class AutocompleteAddressActivity extends AppCompatActivity implements On
         // Start the autocomplete intent.
         Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields)
                 .build(this);
-        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                Place place = Autocomplete.getPlaceFromIntent(data);
-                Log.i(TAG, "Place: " + place.getAddressComponents());
-
-                fillInAddress(place);
-
-            } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
-                // TODO: Handle the error.
-                Status status = Autocomplete.getStatusFromIntent(data);
-                Log.i(TAG, status.getStatusMessage());
-            } else if (resultCode == RESULT_CANCELED) {
-                // The user canceled the operation.
-            }
-            return;
-        }
-        super.onActivityResult(requestCode, resultCode, data);
+        startAutocomplete.launch(intent);
     }
 
     @Override
@@ -149,9 +170,9 @@ public class AutocompleteAddressActivity extends AppCompatActivity implements On
         // and then fill-in the corresponding field on the form.
         // Possible AddressComponent types are documented at https://goo.gle/32SJPM1
         if (components != null) {
-            for(AddressComponent component : components.asList()) {
+            for (AddressComponent component : components.asList()) {
                 String type = component.getTypes().get(0);
-                switch(type) {
+                switch (type) {
                     case "street_number": {
                         address1.insert(0, component.getName());
                         break;
@@ -237,6 +258,19 @@ public class AutocompleteAddressActivity extends AppCompatActivity implements On
         }
     }
 
+    private void saveForm() {
+        Log.d(TAG,"checkProximity = " + String.valueOf(checkProximity));
+        if (checkProximity) {
+            checkLocationPermissions();
+        } else {
+            Toast.makeText(
+                    this,
+                    R.string.autocomplete_skipped_message,
+                    Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
     private void clearForm() {
         address1Field.setText("");
         address2Field.getText().clear();
@@ -248,5 +282,82 @@ public class AutocompleteAddressActivity extends AppCompatActivity implements On
             mapPanel.setVisibility(View.GONE);
         }
         address1Field.requestFocus();
+    }
+
+    // Register the permissions callback, which handles the user's response to the
+    // system permissions dialog. Save the return value, an instance of
+    // ActivityResultLauncher, as an instance variable.
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    Log.d(TAG, "fine location permission granted upon request");
+                    getAndCompareLocations();
+                } else {
+                    Log.d(TAG, "fine location permission denied");
+                    Toast.makeText(
+                            this,
+                            R.string.autocomplete_denied_message,
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
+            });
+
+    private void checkLocationPermissions() {
+        Toast.makeText(
+                getApplicationContext(),
+                R.string.autocomplete_progress_message,
+                Toast.LENGTH_SHORT)
+                .show();
+        if (ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "fine location permission already granted");
+            getAndCompareLocations();
+        } else {
+            requestPermissionLauncher.launch(
+                    ACCESS_FINE_LOCATION);
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getAndCompareLocations() {
+        // TODO: Detect and handle if user has entered or modified the address manually and update
+        // the coordinates variable to the Lat/Lng of the manually entered address. May use
+        // Geocoding API to convert the manually entered address to a Lat/Lng.
+        LatLng enteredLocation = coordinates;
+        map.setMyLocationEnabled(true);
+
+        FusedLocationProviderClient fusedLocationClient =
+                LocationServices.getFusedLocationProviderClient(this);
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, location -> {
+                    // Got last known location. In some rare situations this can be null.
+                    if (location == null) {
+                        return;
+                    }
+
+                    deviceLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    Log.d(TAG, "device location = " + deviceLocation.toString());
+                    Log.d(TAG, "entered location = " + enteredLocation.toString());
+
+                    // Use the computeDistanceBetween function in the Maps SDK for Android Utility Library
+                    // to use spherical geometry to compute the distance between two Lat/Lng points.
+                    double distanceInMeters = computeDistanceBetween(deviceLocation, enteredLocation);
+                    if (distanceInMeters <= acceptableProximity) {
+                        Log.d(TAG, "location matched");
+                        Toast.makeText(
+                                getApplicationContext(),
+                                R.string.autocomplete_match_message,
+                                Toast.LENGTH_SHORT)
+                                .show();
+                    } else {
+                        Log.d(TAG, "location not matched");
+                        Toast.makeText(
+                                getApplicationContext(),
+                                R.string.autocomplete_nomatch_message,
+                                Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
     }
 }
