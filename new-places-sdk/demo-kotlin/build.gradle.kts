c3c0b78eaa19a353621110706e6c5dceca5fb54b
dkhawk <dkhawk@google.com>
2024-02-06 13:48:03 -0700

    New Places Demo Project Set Up

---
+++ new-places-sdk/demo-kotlin/build.gradle.kts
@@ -0,0 +1,11 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+
+buildscript {
+    dependencies {
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
